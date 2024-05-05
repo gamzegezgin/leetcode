341de47edeeb822ad5fcbbfaf14604097fd5ad9c
Gamze Gezgin <86906473+gamzegezgin@users.noreply.github.com>
2024-05-05 08:19:10 -0300

    Time: 30 ms (76.45%), Space: 51.3 MB (67.82%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -36,13 +36,14 @@ class Solution {
         
         return result;
     }
-}
-
-class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = solution.threeSum(nums);
         System.out.println(result);
     }
+
 }
+
+
+
